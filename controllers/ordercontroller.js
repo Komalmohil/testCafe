1c0e4bca9d02109f2f7728ebb7c57595dfb3a1fc
nitujangra <147487358+nitujangra@users.noreply.github.com>
2026-03-26 14:20:47 +0530

    Fixed merge conflicts 3

--- controllers/ordercontroller.js
+++ controllers/ordercontroller.js
@@ -1,6 +1,6 @@
-const Order = require("../models/Order");
+const Order = require("../models/order");
 const Cart = require("../models/Cart");
-const Product = require("../models/Product");
+const Product = require("../models/product");
 
 /* ================= CHECKOUT (STOCKS UPDATED) ================= */
 const checkout = async (req, res) => {
